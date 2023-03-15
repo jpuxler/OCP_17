163f117f1cf47ba4dad540c2a3729fd429efbc90
Jonathan Puxler <software@puxler.de>
2023-03-15 08:29:33 +0100

    Seite 7

---
+++ src/main/java/chapter_one/Comments.java
@@ -0,0 +1,18 @@
+package chapter_one;
+
+public class Comments {
+
+    // comment until end of line
+
+    /*  Multiple
+     *  line comment
+     */
+
+    /**
+     * Javadoc multiple-line comment
+     * @author Jonathan Puxler
+     */
+
+
+
+}
