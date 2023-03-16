878df9019def6a74656e7a7304e0f8d8e29fc622
Jonathan Puxler <software@puxler.de>
2023-03-16 08:28:00 +0100

    Seite 25

---
+++ src/main/java/chapter_one/Chicken.java
@@ -0,0 +1,10 @@
+package chapter_one;
+
+public class Chicken {
+
+    int numEggs = 12; // initialize on line
+    String name;
+    public Chicken(){
+        name = "Duke"; // initialize in constructor
+    }
+}
---
+++ src/main/java/chapter_one/ClassA.java
@@ -0,0 +1,4 @@
+package chapter_one;
+
+public class ClassA {
+}
